3d440731a5d467d025419238b840c24c8af92721
lve-gh <lve.github@outlook.com>
2024-06-17 00:18:30 +0300

    fixed ci

--- test_projects/project1_Simple/analysis.cpp
+++ test_projects/project1_Simple/analysis.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
-
-using namespace std;
+#include <fstream>
 
 int main() {
-	cout << "1111";
-	int a = 0;
-	cin >> a;
-	return 0;
+    std::ofstream file("123.txt", std::ios::out);
+    
+    if (file.is_open()) {
+        std::cout << "File 123.txt created successfully." << std::endl;
+        file.close();
+    } else {
+        std::cerr << "Unable to create file 123.txt." << std::endl;
+    }
+    
+    return 0;
 }
