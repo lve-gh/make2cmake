95834a3e4112b54fc89a0751f64f8de7631cff62
lve-gh <lve.github@outlook.com>
2024-06-17 20:38:14 +0300

    fixed 5 test

--- test_projects/project5_Compilation/generate_src.py
+++ test_projects/project5_Compilation/generate_src.py
@@ -1,14 +1,10 @@
-import random
-
-def generate_random_number():
-    return random.randint(1, 100)
 
 def generate_src_code():
-    with open('..\src.cpp', 'w') as file:
+    with open('src.cpp', 'w') as file:
         file.write('#include <iostream>\n\n')
         file.write('int main() {\n')
-        file.write('\tint x = {}\n;'.format(generate_random_number()) )
-        file.write('\tint y = {}\n;'.format(generate_random_number()))
+        file.write('\tint x = 50;\n')
+        file.write('\tint y = 50;\n')
         file.write('\tint sum = x + y;\n')
         file.write('\tstd::cout << "Sum of {} and {} is: " << sum << std::endl;\n'.format('x', 'y'))
         file.write('\treturn 0;\n')
--- test_projects/project5_Compilation/src.cpp
+++ test_projects/project5_Compilation/src.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 
 int main() {
-	int x = 43
-;	int y = 41
-;	int sum = x + y;
+	int x = 50;
+	int y = 50;
+	int sum = x + y;
 	std::cout << "Sum of x and y is: " << sum << std::endl;
 	return 0;
 }
